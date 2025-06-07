28afcb74573882875a66037302adcab7df0a11c7
Junior 296 <88599309+Junior296@users.noreply.github.com>
2025-06-07 13:26:16 +0300

    Update wsgi.py

--- personal_blog/wsgi.py
+++ personal_blog/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_blog.settings')
 
-app = get_wsgi_application()
+
+application = get_wsgi_application()
+
+app = application
