88d6a7242f5002e24b24c7615ca424cb783d47e9
Junior 296 <88599309+Junior296@users.noreply.github.com>
2025-06-07 12:10:44 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python3.10 manage.py collectstatic --noinput
+
+# Copy collected static files to the build output directory
+cp -r staticfiles staticfiles_build
